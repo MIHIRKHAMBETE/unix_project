0b3857e433e12133bb4937759de7f1cbaa219c2d
Mihir <mkhambet@mit.edu>
2020-08-26 22:01:54 -0700

    created guessing game script, removedREADME.md (to be made by makefile)

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,31 +1,37 @@
 # Guessing Game implementation
 # by Mihir Khambete
 
-# count the number of files in the current directory
-
+# count the number of files in the current directory 
+# assumes no files contained inside directories contained in the working directory
 filecount=$(ls -1 | wc -l)
-echo "filecount is $filecount"
 
 # function for comparing two integers
 function compare {
  if [[ $1 -lt $2 ]]
  then
-  echo "Your guess is too low!"
+  echo "Your guess is too low! Try again!"
   return 1 
  elif [[ $1 -gt $2 ]]
  then 
-  echo "Your guess is too high!"
+  echo "Your guess is too high! Try again!"
   return 1 
  else
-  echo "Correct!"
-  return 1
+  echo "Correct! You Win!"
+  return 0
  fi
 }
 
 # prompt user for initial guess
 
-echo "Welcome to the guessing game! How many files are in this directory? Please enter a guess"
+echo "Welcome to the guessing game! How many files are in this directory? Please enter an integer guess"
 
-read response
-
-echo "You entered: $response"
+# repeat until user inputs correct answer
+while [[ $filecount -eq $filecount ]]
+do
+ read response
+ compare response filecount
+ if [[ $? -eq 0 ]]
+ then
+  break
+ fi
+done
